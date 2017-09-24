2608dba01a2ef72269e9401e0bec85a022a89d4b
Jovanny Cruz <jovannypcg@yahoo.com>
2017-09-24 14:21:05 -0500

    Refactor Repository model to use Storable module

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,7 @@
 class Repository < ApplicationRecord
-  belongs_to :user
+  include Storable
 
-  validates :gh_id, uniqueness: true
+  belongs_to :user
 
   # Converts a hash of form:
   #   { "id": 123, "name": "public_repo", ... }
@@ -15,13 +15,5 @@ class Repository < ApplicationRecord
         name: repo['name'],
         full_name: repo['full_name'],
         url: repo['html_url'])
-  end
-
-  # Stores the current repository only if it does not exist in
-  # database, otherwise the existing record is returned.
-  def store
-    already_exists = Repository.exists?(gh_id: self.gh_id)
-    already_exists ? Repository.find_by(gh_id: self.gh_id)
-                   : self.class.create(self.attributes)
   end
 end
