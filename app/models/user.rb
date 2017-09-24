56f6c6a6454a9cb83582abe6c1f9b5167bd6b7cf
Jovanny Cruz <jovannypcg@yahoo.com>
2017-09-24 14:20:55 -0500

    Refactor User model to use Storable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :repositories, dependent: :destroy
+  include Storable
 
-  validates :username, uniqueness: true
+  has_many :repositories, dependent: :destroy
 
   # Converts a hash of form:
   #   { "id": 1, "login": "ror", ... }
@@ -15,14 +15,5 @@ class User < ApplicationRecord
         gh_id: user['id'],
         avatar: user['avatar_url'],
         url: user['html_url'])
-  end
-
-  # Stores the current user only if it does not exist in
-  # database, otherwise the existing record is returned.
-  def store
-    already_exists = User.exists?(username: self.username)
-
-    already_exists ? User.find_by(username: self.username)
-                   : self.class.create(self.attributes)
   end
 end
